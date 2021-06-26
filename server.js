a195e6021e73618405527a1c649e40ecdee00743
Nazim Chougule <nazim.chougule@neebal.com>
2021-06-26 20:51:01 +0530

    Changes in server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ mongoose.connect(DB, {
     console.log('DB connection successful!');
 });
 
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Listening on port: ${port}`);
 });
